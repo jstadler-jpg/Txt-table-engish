8876f073421814b4ff8f56892382b943258728a7
jstadler-jpg <jstadler@mills.cz>
2026-05-09 18:49:59 +0200

    Add files via upload

---
+++ Txt tabulky.bat
@@ -0,0 +1,28 @@
+@echo off
+title Txt tabulky
+
+echo.
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+echo I               I                I              I  >> Tabulka.txt
+echo _____________________________________________________________________ >> Tabulka.txt
+start Tabulka.txt
